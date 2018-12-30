f6d86e317ba7debbfc09a9ad0bd87b5f4b410a43
hColleen <hcolleen@gmail.com>
2018-12-30 10:44:24 -0700

    Updates

---
+++ precache-manifest.23eed3de96af82a1606d92114d6893f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9060c6a1f230519278e5dcefabe82c36",
+    "url": "/memory-react/static/media/science-14272_960_720.9060c6a1.jpg"
+  },
+  {
+    "revision": "64732fe8352865709903cf62c221a295",
+    "url": "/memory-react/static/media/brain-solid.64732fe8.svg"
+  },
+  {
+    "revision": "98abbd804fbf0ae501f6",
+    "url": "/memory-react/static/js/runtime~main.98abbd80.js"
+  },
+  {
+    "revision": "524de92c7b9dbc6bf0a0",
+    "url": "/memory-react/static/js/main.524de92c.chunk.js"
+  },
+  {
+    "revision": "5b7f36bb20fe43382e6b",
+    "url": "/memory-react/static/js/1.5b7f36bb.chunk.js"
+  },
+  {
+    "revision": "524de92c7b9dbc6bf0a0",
+    "url": "/memory-react/static/css/main.a3c4c741.chunk.css"
+  },
+  {
+    "revision": "285addfab3583426f1eb215e7ef06c1b",
+    "url": "/memory-react/index.html"
+  }
+];
