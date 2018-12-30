2f85f2d2f222ec3c0d465b894fe1add47aa0da6d
hColleen <hcolleen@gmail.com>
2018-12-30 10:09:38 -0700

    add back image to card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactCardFlip from "react-card-flip";
 
+
 const Card = ({ id, isFlipped, handleClick, cardNumber }) => (
   <ReactCardFlip isFlipped={isFlipped} flipSpeedBackToFront={1} flipSpeedFrontToBack={1} >
     <button id={id} className={`card card-front ${cardNumber !== -1 ? "" : "hide-card"}`} onClick={handleClick} key="front">
-      
+
     </button>
 
     <button id={id} className={`card card-back ${cardNumber !== -1 ? "" : "hide-card"}`} onClick={handleClick} key="back">
-      <i className ={`fas ${ cardNumber }`}></i>
+      <i className ={`fas ${ cardNumber } fa-3x`}></i>
     </button>
   </ReactCardFlip>
 );
--- src/components/card/GameOver.jsx
+++ src/components/card/GameOver.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const GameOver = ({ restartGame }) => (
   <div className="justify-center">
     <h1>Game Over!</h1>
-    <h3>If you enjoyed playing this game, follow me @iamkenec for more...</h3>
+    <h3>Congratulations!</h3>
     <button className="restart-button" onClick={restartGame}>Restart Game</button>
   </div>
 );
